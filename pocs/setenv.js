f94d1a1a747823ad7a52f88df114969ca854322e
w <none>
2020-11-02 00:23:29 +0000

    pocs: add setenv

---
+++ pocs/setenv.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+
+(async function () {
+  try {
+    const res = await fetch(`https://api.glitch.com/projects/${process.env.G_PROJECT_DOMAIN}/setenv`, {
+      method: 'POST',
+      headers: {
+        'Authorization': process.env.G_PERSISTENT_TOKEN,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        env: {
+          CLIENT_TIME: '' + Date.now(),
+        },
+      }),
+    });
+    console.log(res);
+    console.log(res.headers);
+    if (!res.ok) throw new Error('response not ok ' + res.status)
+    const body = await res.json();
+    console.log(body);
+  } catch (e) {
+    console.error(e);
+  }
+})();
