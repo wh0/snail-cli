a181391190c94d69601c45b9ce1d8a806f4f9f16
w <none>
2021-01-10 20:36:48 +0000

    pocs: some web url printers

---
+++ pocs/web-edit.js
@@ -0,0 +1,1 @@
+console.log(`https://glitch.com/edit/#!/${process.env.G_PROJECT_DOMAIN}`);
---
+++ pocs/web-term.js
@@ -0,0 +1,1 @@
+console.log(`https://api.glitch.com/${process.env.G_PROJECT_DOMAIN}/console/${process.env.G_PERSISTENT_TOKEN}/`);
